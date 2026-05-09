7772f18f07c4891d99a4860db9b73177745e42de
Alisson Oliveira <bukalyze@hotmail.com>
2026-05-09 10:56:33 -0300

    feat: adicionado CriarPedidoHandler e CriarPedidoCommand junto da camada Application.

---
+++ MeusPedidos/src/MeusPedidos.Application/UseCases/Pedidos/CriarPedido/CriarPedidoCommand.cs
@@ -0,0 +1,7 @@
+﻿namespace MeusPedidos.Application.UseCases.Pedidos.CriarPedido;
+
+public class CriarPedidoCommand
+{
+    public Guid ClienteId { get; set; }
+    public Guid FormaDePagamentoId  { get; set; }
+}
---
+++ MeusPedidos/src/MeusPedidos.Application/UseCases/Pedidos/CriarPedido/CriarPedidoHandler.cs
@@ -0,0 +1,25 @@
+﻿
+
+using MeusPedidos.Domain.Entities;
+using MeusPedidos.Domain.Interfaces;
+
+namespace MeusPedidos.Application.UseCases.Pedidos.CriarPedido;
+
+public class CriarPedidoHandler
+{
+    private readonly IPedidoRepository _pedidoRepository;
+
+    public CriarPedidoHandler(IPedidoRepository pedidoRepository)
+    {
+        _pedidoRepository = pedidoRepository;
+    }
+
+    public async Task<Guid> Handle(CriarPedidoCommand command, CancellationToken cancellationToken)
+    {
+        var pedido = Pedido.Criar(command.ClienteId, command.FormaDePagamentoId);
+        await _pedidoRepository.AdicionarAsync(pedido, cancellationToken);
+
+        return pedido.Id;
+    }
+}
+
