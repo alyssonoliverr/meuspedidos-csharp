e8eb38f3c46c7f97c4a9de1dad44136a6476f228
Alisson Oliveira <bukalyze@hotmail.com>
2026-05-24 18:41:00 -0300

    adicionado readme

---
+++ MeusPedidos/src/MeusPedidos.Infrastructure/Migrations/20260517225003_AdicionadoFKClientes&FormaDePagamento.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MeusPedidos.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdicionadoFKClientesFormaDePagamento : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Pedidos_ClienteId",
+                table: "Pedidos",
+                column: "ClienteId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pedidos_FormaDePagamentoId",
+                table: "Pedidos",
+                column: "FormaDePagamentoId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Pedidos_Clientes_ClienteId",
+                table: "Pedidos",
+                column: "ClienteId",
+                principalTable: "Clientes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Pedidos_FormasDePagamento_FormaDePagamentoId",
+                table: "Pedidos",
+                column: "FormaDePagamentoId",
+                principalTable: "FormasDePagamento",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Pedidos_Clientes_ClienteId",
+                table: "Pedidos");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Pedidos_FormasDePagamento_FormaDePagamentoId",
+                table: "Pedidos");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Pedidos_ClienteId",
+                table: "Pedidos");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Pedidos_FormaDePagamentoId",
+                table: "Pedidos");
+        }
+    }
+}
